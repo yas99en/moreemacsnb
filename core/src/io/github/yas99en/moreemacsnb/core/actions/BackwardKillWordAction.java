18b7dfb83cfd0762752df534cecce24d5a28afe8
yas99en <yas99en@gmail.com>
2015-12-06 20:47:57 +0900

    Added BackwardKillWordAction.java. Modified Bundle.properties and MoreEmacs-keybindings.xml.

---
+++ core/src/io/github/yas99en/moreemacsnb/core/actions/BackwardKillWordAction.java
@@ -0,0 +1,30 @@
+package io.github.yas99en.moreemacsnb.core.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.text.Caret;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import org.netbeans.api.editor.EditorActionRegistration;
+
+
+@EditorActionRegistration(name="io-github-yas99en-moreemacsnb-core-actions-BackwardKillWordAction")
+public final class BackwardKillWordAction extends MoreEmacsAction {
+
+    public BackwardKillWordAction() {
+        super("backward-kill-word");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e, JTextComponent target) {
+        if (!target.isEditable()) {
+            return;
+        }
+
+        Document doc = target.getDocument();
+        Caret caret = target.getCaret();
+        int current = caret.getDot();
+        int prev = BackwardWordAction.getPreviousWordPosition(doc, current);
+        caret.moveDot(prev);
+        target.cut();
+    }
+}
