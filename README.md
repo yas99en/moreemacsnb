370be79685bbc7fb3a928ebc228670ebd3362648
yas99en <yas99en@gmail.com>
2015-12-17 23:44:14 +0900

    fix keyboard-quit

--- README.md
+++ README.md
@@ -56,7 +56,7 @@
 |M-S-r|in-place-refactoring|rebind|
 |C-t|MoreEmacs transpose-chars|Sorry, the display may get corrupted.|
 |M-t|MoreEmacs transpose-words||
-|C-g|MoreEmacs keyboard-quit|send Escape key down/up events after 500ms.You should release ctrl key after C-g.|
+|C-g|MoreEmacs keyboard-quit|dispatch Escape key down/up events.|
 |C-l|adjust-window-center|rebind|
 
 ### Menu Bindings
--- core/src/io/github/yas99en/moreemacsnb/core/actions/KeyboardQuitAction.java
+++ core/src/io/github/yas99en/moreemacsnb/core/actions/KeyboardQuitAction.java
@@ -28,20 +28,14 @@
  */
 package io.github.yas99en.moreemacsnb.core.actions;
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.Timer;
+import static java.awt.event.KeyEvent.KEY_PRESSED;
+import static java.awt.event.KeyEvent.KEY_RELEASED;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.editor.EditorActionRegistration;
-import org.netbeans.editor.BaseKit;
-import org.netbeans.editor.Utilities;
 
 /**
  *
@@ -56,59 +50,17 @@ public class KeyboardQuitAction extends MoreEmacsAction {
     @Override
     public void actionPerformed(ActionEvent e, JTextComponent target) {
         System.out.println("KeyboardQuitAction.actionPerformed()");
-        
-        if(escape(e, target)) {
-            return;
-        }
-
-        try {
-            Robot robot = new java.awt.Robot();
-            Timer timer = new Timer(500, ev -> {
-                robot.keyPress(KeyEvent.VK_ESCAPE);
-                robot.keyRelease(KeyEvent.VK_ESCAPE);
-            });
-            timer.setRepeats(false);
-            timer.start();
-        } catch (AWTException ex) {
-            Logger.getLogger(KeyboardQuitAction.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private boolean escape(ActionEvent e, JTextComponent target) {
-        BaseKit kit = Utilities.getKit(target);
-        if(kit == null) {
-            return false;
-        }
-
-        Action action = kit.getActionByName("escape");
-        if (action != null) {
-            action.actionPerformed(e);
-            return true;
-        }
-        action = kit.getActionByName("Escape");
-        if (action != null) {
-            action.actionPerformed(e);
-            return true;
-        }
-        
-        ActionMap actionMap = target.getActionMap();
-
-        System.out.println("actionMap: " + Arrays.asList(actionMap.keys()));
-
-        action = actionMap.get("escape");
-        if (action != null) {
-            action.actionPerformed(e);
-            return true;
-        }
-
-        action = actionMap.get("Escape");
-        if (action != null) {
-            action.actionPerformed(e);
-            return true;
-        }
-
-        System.out.println("action not found");
-        return false;
+        EventQueue queue = java.awt.Toolkit.getDefaultToolkit().getSystemEventQueue();
+        queue.postEvent(escapeKeyEvent(target, KEY_PRESSED));
+        queue.postEvent(escapeKeyEvent(target, KEY_RELEASED));
     }
     
+    private KeyEvent escapeKeyEvent(Component source, int id) {
+        return new KeyEvent(source, 
+                id, 
+                System.currentTimeMillis(),
+                0, // no modifier
+                KeyEvent.VK_ESCAPE,
+                KeyEvent.CHAR_UNDEFINED);
+    }
 }
